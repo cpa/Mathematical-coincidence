37c4c71619bac0978f17aacae4c4141d511643be
Charles-Pierre Astolfi <350757+cpa@users.noreply.github.com>
2021-05-14 16:53:22 +0200

    2021 update

--- formula.hs
+++ formula.hs
@@ -7,6 +7,8 @@ module Formula
     ,simplify
     ,toLatex
     ,writeLatexDocument
+    ,writeHTMLDocument
+    ,writeStdout
 ) where
 
 import Data.Char (intToDigit,digitToInt)
@@ -15,7 +17,7 @@ import Data.List (partition)
 
 data Formula = Pi
              | Exp1
-			 | Phi
+             | Phi
              | Integer Int
              | Log Formula
              | Exp Formula Formula
@@ -30,7 +32,6 @@ data Formula = Pi
 
 zero = Integer 0
 one = Integer 1
-                         
 
 eval :: Formula -> Double
 eval fs = 
@@ -52,36 +53,38 @@ eval fs =
 simplify :: Formula -> Formula
 simplify fs = 
     case fs of
-      Log (Exp1) -> one
-      Exp one _ -> one
-      Exp a one -> simplify a
+      Log Exp1 -> (Integer 1)
+      Exp (Integer 1) _ -> (Integer 1)
+      Exp a (Integer 1) -> simplify a
       Log (Exp Exp1 f) -> simplify f
       Exp Exp1 (Log f) -> simplify f
-      Times zero f -> zero
-      Times f zero -> zero
-      Plus zero f -> simplify f
-      Plus f zero -> simplify f
-      Minus f zero -> simplify f
-      Quotient zero f -> simplify zero
-      Quotient a b -> if a==b then simplify one else fs
-      Log one -> zero
-      Log (Times f Exp1) -> Plus one $ Log $ simplify f
-      Log (Times Exp1 f) -> Plus one $ Log $ simplify f
-      Exp _ zero -> one
+      Times (Integer 0) f -> (Integer 0)
+      Times f (Integer 0) -> (Integer 0)
+      Plus (Integer 0) f -> simplify f
+      Plus f (Integer 0) -> simplify f
+      Minus f (Integer 0) -> simplify f
+      Quotient (Integer 0) f -> simplify (Integer 0)
+      Quotient a b -> if a==b then (Integer 1) else fs
+      Log (Integer 1) -> (Integer 0)
+      Log (Times f Exp1) -> Plus (Integer 1) $ Log $ simplify f
+      Log (Times Exp1 f) -> Plus (Integer 1) $ Log $ simplify f
+      Exp _ (Integer 0) -> (Integer 1)
       Log f -> Log $ simplify f
       Exp a b -> Exp (simplify a) (simplify b)
       Minus a b -> Minus (simplify a) (simplify b)
       Plus a b -> Plus (simplify a) (simplify b)
       Times a b -> Times (simplify a) (simplify b)
       Quotient a b -> Quotient (simplify a) (simplify b)
+      Sqrt (Integer 1) -> Integer 1
+      Sqrt (Integer 0) -> Integer 0
       Sqrt f -> Sqrt $ simplify f
-      Cos Pi -> Minus zero one
-      Cos (Minus zero Pi) -> one
+      Cos Pi -> Minus (Integer 0) (Integer 1)
+      Cos (Minus (Integer 0) Pi) -> (Integer 1)
       Cos f -> Cos $ simplify f
-      Sin Pi -> zero
-      Sin (Minus zero Pi) -> zero
+      Sin Pi -> (Integer 0)
+      Sin (Minus (Integer 0) Pi) -> (Integer 0)
       Sin f -> Sin $ simplify f
-      _ -> fs
+      fs -> fs
 
 toLatex :: Formula -> String
 toLatex fs = 
@@ -89,15 +92,15 @@ toLatex fs =
       Pi -> "\\pi"
       Exp1 -> "e"
       Phi -> "\\phi"
-      Log f -> "\\log{"++(toLatex f)++"}"
-      Exp a b -> "{"++(toLatex a)++"}^{"++(toLatex b)++"}"
+      Log f -> "\\left(\\ln{"++(toLatex f)++"}\\right)"
+      Exp a b -> "\\left({"++(toLatex a)++"}\\right)^{"++(toLatex b)++"}"
       Plus a b -> "\\left("++(toLatex a)++" + "++(toLatex b)++"\\right)"
       Minus a b -> "\\left("++(toLatex a)++" - "++(toLatex b)++"\\right)"
       Times a b -> "\\left("++(toLatex a)++" "++(toLatex b)++"\\right)"
       Quotient a b -> "\\frac{"++(toLatex a)++"}{"++(toLatex b)++"}"
       Sqrt f -> "\\sqrt{"++(toLatex f)++"}"
-      Cos f -> "\\cos{"++(toLatex f)++"}"
-      Sin f -> "\\sin{"++(toLatex f)++"}"
+      Cos f -> "\\left(\\cos{"++(toLatex f)++"}\\right)"
+      Sin f -> "\\left(\\sin{"++(toLatex f)++"}\\right)"
       Integer n -> [intToDigit n]
 
 writeLatexDocument :: [(Formula,Double)] -> String -> IO ()
@@ -112,3 +115,20 @@ writeLatexDocument fs name =
                 (ones,others) = partition (\(_,x) -> (abs x) > 0.9 && (abs x) < 1.1) $ fs
                 contentsOnes = concat $ map (\(f,v) -> "$$"++(toLatex f)++" = "++(show v)++"$$\n")  ones
                 contentsOthers = concat $ map (\(f,v) -> "$$"++(toLatex f)++" = "++(show v)++"$$\n")  others
+
+writeHTMLDocument :: [(Formula,Double)] -> String -> IO ()
+writeHTMLDocument fs name =
+  do
+    outh <- openFile name WriteMode
+    hPutStrLn outh $ header ++ contentsOthers ++ onesSeparator ++ contentsOnes ++ footer
+    hClose outh
+      where header = "<html>\n<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script><script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script><h2>Coincidences?</h2>"
+            onesSeparator = "<h2>Ones</h2>"
+            footer = ""
+            (ones,others) = partition (\(_,x) -> (abs x) > 0.9 && (abs x) < 1.1) $ fs
+            contentsOnes = concat $ map (\(f,v) -> "$$"++(toLatex f)++" = "++(show v)++"$$\n")  ones
+            contentsOthers = concat $ map (\(f,v) -> "$$"++(toLatex f)++" = "++(show v)++"$$\n")  others
+
+writeStdout :: [(Formula,Double)] -> IO ()
+writeStdout fs = do
+  mapM_ print fs
